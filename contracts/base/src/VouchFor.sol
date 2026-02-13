7c5f6da325a5b264116d072034e0b7c00cd779c3
solidworkssa <solidworkssa@gmail.com>
2026-02-13 20:14:05 +0100

    feat(VouchFor): enhanced solidity logic

--- contracts/base/src/VouchFor.sol
+++ contracts/base/src/VouchFor.sol
@@ -1,22 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/// @title VouchFor Contract
+/// @notice Social vouching and trust network graph.
 contract VouchFor {
-    mapping(address => uint256) public data;
-    uint256 public counter;
 
-    event DataStored(address indexed user, uint256 value);
-
-    function store(uint256 value) external {
-        data[msg.sender] = value;
-        emit DataStored(msg.sender, value);
+    mapping(address => mapping(address => bool)) public vouches;
+    mapping(address => uint256) public vouchCount;
+    
+    function vouch(address _for) external {
+        require(!vouches[msg.sender][_for], "Already vouched");
+        vouches[msg.sender][_for] = true;
+        vouchCount[_for]++;
+    }
+    
+    function unvouch(address _for) external {
+        require(vouches[msg.sender][_for], "Not vouched");
+        vouches[msg.sender][_for] = false;
+        vouchCount[_for]--;
     }
 
-    function retrieve(address user) external view returns (uint256) {
-        return data[user];
-    }
-
-    function incrementCounter() external {
-        counter++;
-    }
 }
